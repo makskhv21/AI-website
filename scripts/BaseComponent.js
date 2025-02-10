e91f369667aac6b82ac55c45fb4a77db798dd016
repo <khvits21@gmail.com>
2025-02-10 12:52:50 +0200

    feat: Add class Tabs for switching tabs

---
+++ scripts/BaseComponent.js
@@ -0,0 +1,32 @@
+class BaseComponent {
+    constructor() {
+      if (this.constructor === BaseComponent) {
+        throw new Error('Неможливо створити екземпляр абстрактного класа BaseComponent!')
+      }
+    }
+  
+    getProxyState(initialState) {
+      return new Proxy(initialState, {
+        get: (target, prop) => {
+          return target[prop]
+        },
+        set: (target, prop, newValue) => {
+          const oldValue = target[prop]
+  
+          target[prop] = newValue
+  
+          if (newValue !== oldValue) {
+            this.updateUI()
+          }
+  
+          return true
+        },
+      })
+    }
+  
+    updateUI() {
+      throw new Error('Необхідно реалізувати метод updateUI!')
+    }
+  }
+  
+  export default BaseComponent
